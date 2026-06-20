c2d3814e82a69e6f41e8a1a939cc41c76243ea9e
Bedram Tamang <tmgbedu@gmail.com>
2026-06-20 12:10:58 -0700

    test(vite-app): rename TestWebRoutes to TestHomeController

--- example/vite-app/tests/test_web_routes.py
+++ example/vite-app/tests/test_web_routes.py
@@ -3,7 +3,7 @@ from fastapi_startkit.fastapi.testing import HttpTestCase
 from tests.test_case import TestCase
 
 
-class TestWebRoutes(TestCase, HttpTestCase):
+class TestHomeController(TestCase, HttpTestCase):
     async def test_health_endpoint_returns_ok(self):
         response = await self.get("/api/health")
 
